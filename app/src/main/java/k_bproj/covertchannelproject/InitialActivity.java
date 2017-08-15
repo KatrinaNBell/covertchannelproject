580fcefca3848bd7e151dbb2b35b78bef8c46325
KatrinaNBell <Katrina.Bell.2015@live.rhul.ac.uk>
2017-08-15 18:15:32 +0000

    Initial commit

---
+++ app/src/main/java/k_bproj/covertchannelproject/InitialActivity.java
@@ -0,0 +1,13 @@
+package k_bproj.covertchannelproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+    }
+}
